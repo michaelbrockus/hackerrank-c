b8105e84e7dc80efa8e40d138d04382de19ce0ea
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-16 22:54:23 -0800

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,21 @@
 // main is where program execution starts
 int main(void)
 {
-    printf("%s", "Lets do some HackerRank stuff");
+	int a, b;
+    float c, d;
+
+    //
+    // here we ask the user to enter two integers
+    // then print sum and abs of the two values
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d %d\n", a + b, a - b);
+
+    //
+    // here we ask the user to enter two floats
+    // then print sum and abs of the two values
+    scanf("%f", &c);
+    scanf("%f", &d);
+    printf("%.1f %.1f", c + d, c - d);
     return EXIT_SUCCESS;
 } // end of function main
