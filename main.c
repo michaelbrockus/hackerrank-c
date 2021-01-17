341f1993841e251a88f89c300fcca3b93ff403ce
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-16 23:00:11 -0800

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,57 @@
 // main is where program execution starts
 int main(void)
 {
-    printf("%s", "Lets do some HackerRank stuff");
+    int i, j, k;
+    int m, n;
+    scanf("%d", &n);
+
+  	// Complete the code to print the pattern.
+    
+    scanf("%d", &n);
+    m = 2 * n + 1;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = n; j > n - i; j--)
+        {
+            printf("%d ", j);
+        } // end for
+
+        for (j = 1; j <= n - i; j++)
+        {
+            printf("%d ", n - i + 1);
+        } // end for
+
+        for (j = n - i; j >= 1; j--)
+        {
+            printf("%d ", n - i + 1);
+        } // end for
+
+        k = n - i + 2;
+        for (j = 1; j <= i - 1; j++)
+        {
+            printf("%d ", k), k++;
+        } // end for
+        putchar('\n');
+    } // end for
+
+    for (i = 1; i < n; i++)
+    {
+        for (j = n; j >= i + 1; j--)
+        {
+            printf("%d ", j);
+        } // end for
+
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("%d ", i + 1);
+        } // end for
+
+        for (j = i + 1; j <= n; j++)
+        {
+            printf("%d ", j);
+        } // end for
+        putchar('\n');
+    } // end for
+
     return EXIT_SUCCESS;
 } // end of function main
