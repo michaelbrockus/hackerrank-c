68e1b4b2c089052301f6178d08401351dc954b79
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-17 00:10:57 -0800

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,17 @@
 // gmail: <michaelbrockus@gmail.com>
 //
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 // main is where program execution starts
 int main(void)
 {
-    printf("%s", "Lets do some HackerRank stuff");
+    char x[5];
+    scanf("%s", x);
+
+    //
+    // Complete the code to calculate the sum of the five digits on n.
+    printf("%d", x[0] - '0' + x[1] - '0' + x[2] - '0' + x[3] - '0' + x[4] - '0');
     return EXIT_SUCCESS;
 } // end of function main
