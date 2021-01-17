44ffc73d647f457a9ef11b0a624ca5c9e6a56fbe
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-17 00:19:42 -0800

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//
+// Complete this function
+//
+void update(int *a, int *b)
+{
+    int temp = *a;
+    *a = *a + *b;
+    *b = temp > *b ? temp - *b : *b - temp;   
+} // end of function update
+
 // main is where program execution starts
 int main(void)
 {
-    printf("%s", "Lets do some HackerRank stuff");
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
     return EXIT_SUCCESS;
 } // end of function main
